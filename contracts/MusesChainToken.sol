8470a7ae1b6abf0cfaae048f41847adf95f5c1cf
任佶 <renji@meikeland.com>
2018-03-07 02:16:59 +0800

    only whitelisted address can mint

--- contracts/MusesChainToken.sol
+++ contracts/MusesChainToken.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.4.18;
-import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "zeppelin-solidity/contracts/token/ERC20/CappedToken.sol";
+import "./Whitelist.sol";
 
 /**
  * @title SimpleToken
@@ -7,19 +8,24 @@ import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
  * Note they can later distribute these tokens as they wish using `transfer` and other
  * `StandardToken` functions.
  */
-contract MusesChainToken is StandardToken {
+contract MusesChainToken is CappedToken, Whitelist {
   string public name = "MusesChainToken";
   string public symbol = "MC";
   uint8 public decimals = 18;
 
-  uint256 public INITIAL_SUPPLY = 10000 * (10 ** uint256(decimals));
+  uint256 public INITIAL_SUPPLY = 1050000000;
+  uint256 public CAP_SUPPLY = 2100000000;
 
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
    */
-  function MusesChainToken() public {
+  function MusesChainToken() public CappedToken(CAP_SUPPLY) {
     totalSupply_ = INITIAL_SUPPLY;
     balances[msg.sender] = INITIAL_SUPPLY;
     Transfer(0x0, msg.sender, INITIAL_SUPPLY);
+  }
+
+  function mint(address _to, uint256 _amount) onlyOwner canMint onlyWhitelisted(_to) public returns (bool) {
+    return super.mint(_to, _amount);
   }
 }
---
+++ contracts/Whitelist.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.4.18;
+
+
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
+
+
+/**
+ * @title Whitelist
+ * @dev The Whitelist contract has a whitelist of addresses, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Whitelist is Ownable {
+  mapping(address => bool) public whitelist;
+
+  event WhitelistedAddressAdded(address addr);
+  event WhitelistedAddressRemoved(address addr);
+
+  /**
+   * @dev Throws if called by any account that's not whitelisted and not an owner.
+   */
+  modifier onlyWhitelisted(address target) {
+    require(whitelist[target] || owner == msg.sender);
+    _;
+  }
+
+  /**
+   * @dev add an address to the whitelist
+   * @param addr address
+   */
+  function addToWhitelist(address addr) onlyOwner public returns(bool) {
+    if (whitelist[addr]) {
+      return false;
+    }
+    whitelist[addr] = true;
+    WhitelistedAddressAdded(addr);
+    return true;
+  }
+
+  /**
+   * @dev remove an address from the whitelist
+   * @param addr address
+   */
+  function removeFromWhitelist(address addr) onlyOwner public returns(bool) {
+    if (!whitelist[addr]) {
+      return false;
+    }
+    whitelist[addr] = false;
+    WhitelistedAddressRemoved(addr);
+    return true;
+  }
+
+}
